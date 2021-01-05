6d2bab9d6e32f18224e13ba6ae096158d9eb8b47
Ozzie Harb <ozharb@gmail.com>
2021-01-05 15:10:30 -0800

    first commit

---
+++ src/store.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+const bookmarks = [
+  {
+    'id': 'e71ecfc7-337e-4800-a62c-2a39d8f3ae8f',
+    'title': 'music',
+    'url': 'https://www.spotify.com',
+    'description': 'tune for loons',
+    'rating': '4'
+  },
+  {
+    'id': '458341e1-33bd-4142-813b-b41660f8889a',
+    'title': 'new',
+    'url': 'https://www.nytimes.com',
+    'description': 'news you can use',
+    'rating': '5'
+  },
+  {
+    'id': 'c6c62528-942f-4566-86b7-a4ae0da8cdc2',
+    'title': 'radio',
+    'url': 'https://www.kexp.org',
+    'description': 'radio you can listen to',
+    'rating': '5'
+  },
+  {
+    'id': '7065c3c9-e8ee-4cef-bc5e-8cf9a7e9a04f',
+    'title': 'legos',
+    'url': 'https://www.lego.com',
+    'description': 'blocks of fun',
+    'rating': '5'
+  }
+];
+  
+  
+module.exports = { bookmarks };
